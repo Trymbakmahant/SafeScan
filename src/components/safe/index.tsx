1d4d6f38d8e5e5bcd94b766be8e1d740b46fd5d9
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-10-15 10:20:33 +0530

    update safe page useEffect error

--- src/components/safe/index.tsx
+++ src/components/safe/index.tsx
@@ -1,74 +1,83 @@
-import { Web3AuthModalPack, Web3AuthConfig } from "@safe-global/auth-kit";
-import { Web3AuthOptions } from "@web3auth/modal";
+import { Web3AuthModalPack } from "@safe-global/auth-kit";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from "@web3auth/base";
+import { Web3AuthOptions } from "@web3auth/modal";
 
-// https://web3auth.io/docs/sdk/pnp/web/modal/initialize#arguments
-const options: Web3AuthOptions = {
-  clientId:
-    "BLJwVWQUu4eMOWzF2cMrhmBrKlO7m4KOr39418nSw28Z5q446Nek-VUdpP9ZcwCWM6C5TniHQrQYHBvE8NNMlwQ", // https://dashboard.web3auth.io/
-  web3AuthNetwork: "testnet",
-  chainConfig: {
-    chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: "0x13881",
-    // https://chainlist.org/
-    rpcTarget:
-      "https://polygon-mumbai.g.alchemy.com/v2/E_OwgnjNJnCelsz2bUWwNlZ880TmumS-",
-  },
-  uiConfig: {
-    theme: "dark",
-    loginMethodsOrder: ["google", "facebook"],
-  },
-};
+// // Allow to login and get the derived EOA
+// await web3AuthModalPack.signIn();
 
-// https://web3auth.io/docs/sdk/pnp/web/modal/initialize#configuring-adapters
-const modalConfig = {
-  [WALLET_ADAPTERS.TORUS_EVM]: {
-    label: "torus",
-    showOnModal: false,
-  },
-  [WALLET_ADAPTERS.METAMASK]: {
-    label: "metamask",
-    showOnDesktop: true,
-    showOnMobile: false,
-  },
-};
+// // Logout
+// await web3AuthModalPack.signOut();
 
-// https://web3auth.io/docs/sdk/pnp/web/modal/whitelabel#whitelabeling-while-modal-initialization
-const openloginAdapter = new OpenloginAdapter({
-  loginSettings: {
-    mfaLevel: "mandatory",
-  },
-  adapterSettings: {
-    uxMode: "popup",
-    whiteLabel: {
-      name: "Safe",
+// // Get the provider
+// web3AuthModalPack.getProvider();
+
+import React, { useEffect } from "react";
+
+const Page = () => {
+  const options: Web3AuthOptions = {
+    clientId:
+      "BLJwVWQUu4eMOWzF2cMrhmBrKlO7m4KOr39418nSw28Z5q446Nek-VUdpP9ZcwCWM6C5TniHQrQYHBvE8NNMlwQ",
+    web3AuthNetwork: "testnet",
+    chainConfig: {
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      chainId: "0x13881",
+      rpcTarget:
+        "https://polygon-mumbai.g.alchemy.com/v2/E_OwgnjNJnCelsz2bUWwNlZ880TmumS-",
     },
-  },
-});
+    uiConfig: {
+      theme: "dark",
+      loginMethodsOrder: ["google", "facebook"],
+    },
+  };
 
-const web3AuthConfig: Web3AuthConfig = {
-  txServiceUrl: "https://safe-transaction-goerli.safe.global",
-};
+  const modalConfig = {
+    [WALLET_ADAPTERS.TORUS_EVM]: {
+      label: "torus",
+      showOnModal: false,
+    },
+    [WALLET_ADAPTERS.METAMASK]: {
+      label: "metamask",
+      showOnDesktop: true,
+      showOnMobile: false,
+    },
+  };
 
-// Instantiate and initialize the pack
-const web3AuthModalPack = new Web3AuthModalPack(web3AuthConfig);
+  const openloginAdapter = new OpenloginAdapter({
+    loginSettings: {
+      mfaLevel: "mandatory",
+    },
+    adapterSettings: {
+      uxMode: "popup",
+      whiteLabel: {
+        name: "Safe",
+      },
+    },
+  });
 
-import React from "react";
-
-const index = () => {
+  const web3AuthModalPack = new Web3AuthModalPack({
+    txServiceUrl: "https://safe-transaction-{chain}.safe.global",
+  });
+  const initfunction = async () => {
+    await web3AuthModalPack.init({
+      options,
+      adapters: [openloginAdapter],
+      modalConfig,
+    });
+  };
+  useEffect(() => {
+    initfunction();
+  });
   return (
     <button
       onClick={async () => {
-        await web3AuthModalPack.init({
-          options,
-          adapters: [openloginAdapter],
-          modalConfig,
-        });
+        try {
+          const ans: any = await web3AuthModalPack.signIn();
 
-        // The signIn() method will return the user's Ethereum address
-        // The await will last until the user is authenticated, so while the UI modal is showed
-        const authKitSignData = await web3AuthModalPack.signIn();
+          console.log(ans);
+        } catch (e) {
+          console.log(e);
+        }
       }}
     >
       hello
@@ -76,4 +85,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Page;
