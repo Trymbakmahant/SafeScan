2b34539c0eb54506a8eee3b43b452f5a3b9b7ecf
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-10-16 16:04:34 +0530

    carousel update

---
+++ src/components/UI/RecentTransactionCard/CarouselTogglebar.tsx
@@ -0,0 +1,61 @@
+interface ToggleOption {
+  label: string;
+  value: string;
+}
+import React, { useState } from "react";
+import { Tab, Tabs, Paper, IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+interface CarouselToggleBarProps {
+  options: ToggleOption[];
+  onChange: (value: string) => void;
+}
+
+const CarouselToggleBar: React.FC<CarouselToggleBarProps> = ({
+  options,
+  onChange,
+}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabChange = (event: React.ChangeEvent<{}>, newIndex: number) => {
+    setActiveIndex(newIndex);
+    onChange(options[newIndex].value);
+  };
+
+  const handlePrevClick = () => {
+    const newIndex = Math.max(0, activeIndex - 8);
+    setActiveIndex(newIndex);
+    onChange(options[newIndex].value);
+  };
+
+  const handleNextClick = () => {
+    const newIndex = Math.min(options.length - 1, activeIndex + 8);
+    setActiveIndex(newIndex);
+    onChange(options[newIndex].value);
+  };
+
+  return (
+    <Paper style={{ display: "flex", alignItems: "center" }}>
+      <IconButton onClick={handlePrevClick}>
+        <ArrowBackIcon />
+      </IconButton>
+      <Tabs
+        value={activeIndex}
+        onChange={handleTabChange}
+        indicatorColor="primary"
+        textColor="primary"
+        scrollButtons="auto"
+      >
+        {options.map((option, index) => (
+          <Tab key={index} label={option.label} />
+        ))}
+      </Tabs>
+      <IconButton onClick={handleNextClick}>
+        <ArrowForwardIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default CarouselToggleBar;
--- src/components/UI/RecentTransactionCard/page.tsx
+++ src/components/UI/RecentTransactionCard/page.tsx
@@ -9,11 +9,40 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Card from "@mui/material/Card";
+import CarouselToggleBar from "./CarouselTogglebar";
 import TablePagination from "@mui/material/TablePagination";
 export default function PinnedSubheaderList() {
+  interface ToggleOption {
+    label: string;
+    value: string;
+  }
+  const options: ToggleOption[] = [
+    { label: "Option 1", value: "option1" },
+    { label: "Option 2", value: "option2" },
+    { label: "Option 3", value: "option3" },
+    { label: "Option 4", value: "option4" },
+    { label: "Option 5", value: "option5" },
+    { label: "Option 6", value: "option6" },
+    { label: "Option 7", value: "option7" },
+    { label: "Option 8", value: "option8" },
+    { label: "Option 9", value: "option9" },
+    { label: "Option 10", value: "option10" },
+    { label: "Option 11", value: "option11" },
+    { label: "Option 12", value: "option12" },
+    { label: "Option 13", value: "option13" },
+    { label: "Option 14", value: "option14" },
+    { label: "Option 15", value: "option15" },
+    { label: "Option 16", value: "option16" },
+  ];
   const [alignment, setAlignment] = React.useState("web");
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [selectedOption, setSelectedOption] = React.useState(options[0].value);
+
+  const handleOptionChange = (value: string) => {
+    setSelectedOption(value);
+  };
+
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string
@@ -29,10 +58,10 @@ export default function PinnedSubheaderList() {
     setPage(newPage);
     console.log(newPage);
   };
+
   function createData(sender: string, reciver: number, amount: number) {
     return { sender, reciver, amount };
   }
-
   const rows = [
     createData("Frozen yoghurt", 159, 6.0),
     createData("Ice cream sandwich", 237, 9.0),
@@ -44,29 +73,8 @@ export default function PinnedSubheaderList() {
   return (
     <>
       <Card sx={{ maxWidth: "80%", maxHeight: "40%" }}>
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          aria-label="Platform"
-        >
-          <ToggleButton value="Arbitrum">Arbitrum</ToggleButton>
-          <ToggleButton value="Aurora">Aurora</ToggleButton>
-          <ToggleButton value="Avalanche">Avalanche</ToggleButton>
-          <ToggleButton value="Base">Base</ToggleButton>
-          <ToggleButton value="BaseGoerli">Base Goerli</ToggleButton>
-          <ToggleButton value="BNBSmartChain">BNB Smart Chain</ToggleButton>
-          <ToggleButton value="EthereumMainnet">Ethereum Mainnet</ToggleButton>
-          <ToggleButton value="GnosisChain">Gnosis Chain</ToggleButton>
-          <ToggleButton value="Goerli">Goerli</ToggleButton>
-          <ToggleButton value="Optimism">Optimism</ToggleButton>
-          <ToggleButton value="Polygon">Polygon</ToggleButton>
-          <ToggleButton value="PolygonzkEVM">Polygon zkEVM</ToggleButton>
-          <ToggleButton value="zkSyncEraMainnet">
-            zkSync Era Mainnet
-          </ToggleButton>
-        </ToggleButtonGroup>
+        <CarouselToggleBar options={options} onChange={handleOptionChange} />
+
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
