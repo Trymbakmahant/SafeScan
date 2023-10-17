491d747fbd27fba81845b01c8e40a7731598cce6
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-10-17 11:09:19 +0530

    Update page.tsx

--- src/pages/LandingPage/page.tsx
+++ src/pages/LandingPage/page.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 import List from "../../components/UI/RecentTransactionCard/page";
+import { Box, Grid, Typography } from "@mui/material";
 const LandingPage = () => {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        placeItems: "center",
+      }}
+      mt={4}
+      gap={4}
+    >
+      <Grid
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          placeItems: "center",
+        }}
+      >
+        <Typography
+          style={{
+            fontSize: "35px",
+            color: "white",
+          }}
+        >
+          Welcome to Safe Scan
+        </Typography>
+        <Typography
+          style={{
+            fontSize: "15px",
+            color: "#6496B7",
+          }}
+        >
+          Welcome to Safe Scan
+        </Typography>
+      </Grid>
       <List />
-    </div>
+    </Box>
   );
 };
 
