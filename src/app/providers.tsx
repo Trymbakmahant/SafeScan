74742240455bfd17f443399a3039daaafb9f16c7
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-10-18 20:28:44 +0530

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,39 @@
-'use client'
+"use client";
 
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
+import {
+  RainbowKitProvider,
+  darkTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import * as React from "react";
+import { WagmiConfig } from "wagmi";
 
-import { chains, config } from '../wagmi'
+import { chains, config } from "../wagmi";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+
   return (
     <WagmiConfig config={config}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider
+        coolMode
+        theme={midnightTheme({
+          // accentColor: "#7b3fe4",
+          // accentColorForeground: "white",
+          // borderRadius: "small",
+          // fontStack: "system",
+          // overlayBlur: "small",
+          accentColor: "#fafafa",
+          accentColorForeground: "black",
+          borderRadius: "large",
+          fontStack: "system",
+          overlayBlur: "small",
+        })}
+        chains={chains}
+      >
         {mounted && children}
       </RainbowKitProvider>
     </WagmiConfig>
-  )
+  );
 }
