c0e0373364a6a06e84838dcbc7bf558f0e23f37b
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-10-21 20:31:02 +0530

    landing page updated

--- src/app/components/UI/RecentTransactionCard/ItemList.tsx
+++ src/app/components/UI/RecentTransactionCard/ItemList.tsx
@@ -13,10 +13,26 @@ import {
 } from "@mui/material";
 
 interface Item {
-  id: number;
-  receiver: string;
-  sender: string;
-  amount: string;
+  blockHash: "0xa54e2e48552c7428c6f7b5b748a78d452a3bd23a227e4c992c634d315816d8e5";
+  blockNumber: "18399277";
+  confirmations: "20";
+  contractAddress: "";
+  cumulativeGasUsed: "6659184";
+  from: "0x9d0870e65416c0727cf733704069d9fea5a66be4";
+  functionName: "createProxyWithNonce(address _mastercopy, bytes initializer, uint256 saltNonce)";
+  gas: "261619";
+  gasPrice: "10999913628";
+  gasUsed: "258640";
+  hash: "0xe2cbcb881abb5825f24f97f84439b1cb7acd191b705a0ebdcee3c31309d2747a";
+  input: "0x1688f0b9000000000000000000000000d9db270c1b5e3bd161e8c8503c55ceabee70955200000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000018b52a241b00000000000000000000000000000000000000000000000000000000000000164b63e800d0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000f48f2b2d2a534e402487b3ee7c18c33aec0fe5e400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000009d0870e65416c0727cf733704069d9fea5a66be4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+  isError: "0";
+  methodId: "0x1688f0b9";
+  nonce: "9";
+  timeStamp: "1697898467";
+  to: "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2";
+  transactionIndex: "67";
+  txreceipt_status: "1";
+  value: "0";
 }
 
 interface ItemListProps {
@@ -30,20 +46,28 @@ const ItemList: React.FC<ItemListProps> = ({ items }) => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Receiver</TableCell>
-              <TableCell>Sender</TableCell>
-              <TableCell>Amount</TableCell>
-              <TableCell>Action</TableCell>
+              <TableCell>Transaction Hash</TableCell>
+              <TableCell>Method</TableCell>
+              <TableCell>Block</TableCell>
+              <TableCell>Age</TableCell>
+              <TableCell>From</TableCell>
+              <TableCell>To</TableCell>
+              <TableCell>Value</TableCell>
+              <TableCell>Txn Fee</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {items.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>{item.receiver}</TableCell>
-                <TableCell>{item.sender}</TableCell>
-                <TableCell>{item.amount}</TableCell>
+              <TableRow key={item.blockNumber}>
+                <TableCell>{item.hash}</TableCell>
+                <TableCell>{item.functionName}</TableCell>
+                <TableCell>{item.timeStamp}</TableCell>
+                <TableCell>{item.from}</TableCell>
+                <TableCell>{item.to}</TableCell>
+                <TableCell>{item.value}</TableCell>
+                <TableCell>{item.gas}</TableCell>
                 <TableCell>
-                  <Link href={`/pages/${item.id}`} passHref>
+                  <Link href={`/pages/${item.hash}`} passHref>
                     <Button variant="contained" color="primary" component="a">
                       View Details
                     </Button>
--- src/app/components/UI/RecentTransactionCard/page.tsx
+++ src/app/components/UI/RecentTransactionCard/page.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import ItemList from "./ItemList";
+import axios from "axios";
 import { options } from "../../../constant/AllServiceOption";
 import {
   Tab,
@@ -11,6 +12,7 @@ import {
   Typography,
   TablePagination,
 } from "@mui/material";
+import { Connected } from "../../wagmiCode/Connected";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 interface ToggleOption {
@@ -23,17 +25,14 @@ interface ToggleOption {
 }
 export default function PinnedSubheaderList() {
   const [page, setPage] = React.useState(0);
+  const [flag, setFlag] = React.useState(false);
+  const [itemsToShow, setItemsToShow] = React.useState([]);
   const [items, setItems] = React.useState([]);
-  const rowsPerPage = 8;
+  const rowsPerPage = 10;
 
   const handleChangePage = (event: any, newPage: number) => {
     setPage(newPage); // Ensure this updates the page state correctly.
   };
-  let itemsToShow = items;
-
-  if (items.length > 8) {
-    itemsToShow = items.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
-  }
 
   const [selectedOption, setSelectedOption] = React.useState(options[0].value);
   const [currentPage, setCurrentPage] = React.useState(1);
@@ -50,57 +49,107 @@ export default function PinnedSubheaderList() {
   };
 
   const handlePrevClick = () => {
-    const newIndex = Math.max(0, activeIndex - 8);
+    const newIndex = Math.max(0, activeIndex - 10);
     setActiveIndex(newIndex);
     handleOptionChange(options[newIndex].value);
   };
 
   const handleNextClick = () => {
-    const newIndex = Math.min(options.length - 1, activeIndex + 8);
+    const newIndex = Math.min(options.length - 1, activeIndex + 10);
     setActiveIndex(newIndex);
     handleOptionChange(options[newIndex].value);
   };
 
   const handlePageChange = (newPage: number) => {
     setCurrentPage(newPage);
+    setItemsToShow(items.slice(page * rowsPerPage, (page + 1) * rowsPerPage));
   };
+
+  async function DataApi() {
+    const safeTxHash =
+      "0x596104426ff8fd56e0488099cfe1829b45aaab323af1ef9cf8d610cae7af57ac";
+    const safeAddress = "0xa43624b7472c37B1E1884645a3D04710afCD8eB5";
+
+    const apiKey = "26TJW7XYJ3UUYQ7T9E3PV4TDZR748TR84V";
+    const address = "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2";
+
+    const apiUrl = "https://api.etherscan.io/api";
+    const action = "txlist"; // For normal transactions
+    const module = "account";
+    const startBlock = "0"; // Start from block 0
+    const endBlock = "latest"; // Up to the latest block
+
+    axios
+      .get(apiUrl, {
+        params: {
+          module,
+          action,
+          address,
+          startblock: startBlock,
+          endblock: endBlock,
+          sort: "desc", // To get the most recent transactions first
+          apikey: apiKey,
+        },
+      })
+      .then((response) => {
+        const transactions = response.data.result;
+        setItems(transactions);
+        setItemsToShow(
+          transactions.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+        );
+
+        console.log("Transactions:", itemsToShow);
+        setFlag(true);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  React.useEffect(() => {
+    DataApi();
+  }, []);
 
   return (
     <>
-      <Card sx={{ maxWidth: "80%", maxHeight: "40%" }}>
-        <Paper style={{ display: "flex", alignItems: "center" }}>
-          <IconButton onClick={handlePrevClick}>
-            <ArrowBackIcon />
-          </IconButton>
+      <Connected>
+        {flag && (
+          <Card sx={{ maxWidth: "80%", maxHeight: "40%" }}>
+            <Paper style={{ display: "flex", alignItems: "center" }}>
+              <IconButton onClick={handlePrevClick}>
+                <ArrowBackIcon />
+              </IconButton>
 
-          <Tabs
-            value={activeIndex}
-            onChange={handleTabChange}
-            indicatorColor="primary"
-            textColor="primary"
-            scrollButtons="auto"
-          >
-            {options.map((option, index) => (
-              <Tab key={index} label={option.label} />
-            ))}
-          </Tabs>
+              <Tabs
+                value={activeIndex}
+                onChange={handleTabChange}
+                indicatorColor="primary"
+                textColor="primary"
+                scrollButtons="auto"
+              >
+                {options.map((option, index) => (
+                  <Tab key={index} label={option.label} />
+                ))}
+              </Tabs>
 
-          <IconButton onClick={handleNextClick}>
-            <ArrowForwardIcon />
-          </IconButton>
-        </Paper>
-        <Container>
-          <ItemList items={itemsToShow} />
-          <TablePagination
-            component="div"
-            count={items.length} // Ensure this represents the total number of items.
-            page={page}
-            onPageChange={handleChangePage}
-            rowsPerPage={rowsPerPage}
-            rowsPerPageOptions={[rowsPerPage]}
-          />
-        </Container>
-      </Card>
+              <IconButton onClick={handleNextClick}>
+                <ArrowForwardIcon />
+              </IconButton>
+            </Paper>
+            <Container>
+              <ItemList items={itemsToShow} />
+              <TablePagination
+                component="div"
+                count={items.length} // Ensure this represents the total number of items.
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                rowsPerPageOptions={[rowsPerPage]}
+              />
+            </Container>
+          </Card>
+        )}
+      </Connected>
     </>
   );
 }
